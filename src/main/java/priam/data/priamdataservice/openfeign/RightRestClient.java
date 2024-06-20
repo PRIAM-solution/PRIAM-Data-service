0c1499dc3f9260b9c58fbce5035109fbb38eec08
Thumus <thumus@tutanota.com>
2024-06-20 09:53:27 +0200

    fix: RightRestClient

--- src/main/java/priam/data/priamdataservice/openfeign/RightRestClient.java
+++ src/main/java/priam/data/priamdataservice/openfeign/RightRestClient.java
@@ -3,12 +3,12 @@ package priam.data.priamdataservice.openfeign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import priam.data.priamdataservice.security.FeignClientConfiguration;
 
-//@FeignClient(name = "RIGHT-SERVICE")
-//@FeignClient(name = "RIGHT-SERVICE",
-        //url = "http://localhost:8083",
-        //configuration = FeignClientConfiguration.class)
+@FeignClient(name = "RIGHT-SERVICE",
+        url = "http://localhost:8083",
+        configuration = FeignClientConfiguration.class)
 public interface RightRestClient {
     @GetMapping(path = "api/isAccepted")
     public boolean getIfDataAccessAccepted(@RequestParam int dataSubjectId, @RequestParam int dataId);
